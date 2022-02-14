7791028b3ca16eeffb766da5969ea4a9fbf0c347
DESKTOP-DDSAMEERA <digix.sameera@hotmail.com>
2022-02-14 13:14:31 +0530

    modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {EmpSingleComponent} from "./emp-single/emp-single.component";
+import {EmpMainComponent} from "./emp-main/emp-main.component";
+
+
+const routes: Routes = [
+
+  {
+    path: 'emp',
+    component: EmpMainComponent,
+
+  },
+
+  {
+    path: 'single',
+    component: EmpSingleComponent,
+
+  }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {EmpSingleComponent} from './emp-single/emp-single.component';
+
+import {EmpMainComponent} from './emp-main/emp-main.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpSingleComponent,
+    EmpMainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
